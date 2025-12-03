7a6ab6709ba4b84c0779c6465072560c739ad5a2
Lord Orendain <lrdrndn51103@gmail.com>
2025-12-03 10:54:27 +0800

    Revert "added inventory logger"
    This reverts commit 118d1bd6c1c0d803c9d180dc9995b9a14a206870.

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,14 +1,20 @@
-import process from 'process/browser'
+// src/polyfills.js
 import { Buffer } from 'buffer'
 
-window.process = process
+// Create a minimal process object
+window.process = window.process || {
+  env: {},
+  browser: true,
+  stdout: { isTTY: false },
+  stderr: { isTTY: false },
+  platform: 'browser',
+  version: '',
+  versions: {},
+  nextTick: (fn, ...args) => setTimeout(() => fn(...args), 0),
+  cwd: () => '/',
+  exit: () => {},
+  argv: []
+}
+
 window.Buffer = Buffer
 window.global = window
-
-// Mock TTY for logging libraries
-if (!window.process.stdout) {
-  window.process.stdout = { isTTY: false }
-}
-if (!window.process.stderr) {
-  window.process.stderr = { isTTY: false }
-}
